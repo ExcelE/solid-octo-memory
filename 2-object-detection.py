aacf5fad64b8b2bd81cc22b84f746d233f965484
Excel Espina <eespina002@citymail.cuny.edu>
2019-05-12 23:46:18 -0400

    exploring not closing resources after

--- 2-object-detection.py
+++ 2-object-detection.py
@@ -1,12 +1,13 @@
 import numpy as np
 import cv2, os
 import tensorflow as tf
+import tensorflow.contrib.tensorrt as trt
 from object_detection.utils import label_map_util
 from object_detection.utils import visualization_utils as vis_util
 from object_detection.utils import ops as utils_ops
 
 # Path to frozen detection graph. This is the actual model that is used for the object detection.
-PATH_TO_FROZEN_GRAPH = 'data/frozen_inference_graph.pb'
+PATH_TO_FROZEN_GRAPH = 'data/ssd_mobilenet_v2_coco_trt.pb'
 
 # List of the strings that is used to add correct label for each box.
 PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
@@ -20,41 +21,41 @@ with detection_graph.as_default():
 		od_graph_def.ParseFromString(serialized_graph)
 		tf.import_graph_def(od_graph_def, name='')
 
-config = tf.ConfigProto()
-config.gpu_options.allow_growth=True
+def run_inference_for_single_image(image, graph, sess):
+	# Get handles to input and output tensors
+	ops = tf.get_default_graph().get_operations()
+	all_tensor_names = {output.name for op in ops for output in op.outputs}
+	tensor_dict = {}
+	for key in [
+		'num_detections', 'detection_boxes', 'detection_scores',
+		'detection_classes', 'detection_masks'
+	]:
+		tensor_name = key + ':0'
+		if tensor_name in all_tensor_names:
+			tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(
+				tensor_name)
+	image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
 
-def run_inference_for_single_image(image, graph):
-	with graph.as_default(config=config):
-		with tf.Session() as sess:
-			# Get handles to input and output tensors
-			ops = tf.get_default_graph().get_operations()
-			all_tensor_names = {output.name for op in ops for output in op.outputs}
-			tensor_dict = {}
-			for key in [
-				'num_detections', 'detection_boxes', 'detection_scores',
-				'detection_classes', 'detection_masks'
-			]:
-				tensor_name = key + ':0'
-				if tensor_name in all_tensor_names:
-					tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(
-						tensor_name)
-			image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
+	# Run inference
+	output_dict = sess.run(tensor_dict,
+							feed_dict={image_tensor: image})
 
-			# Run inference
-			output_dict = sess.run(tensor_dict,
-									feed_dict={image_tensor: image})
-
-			# all outputs are float32 numpy arrays, so convert types as appropriate
-			output_dict['num_detections'] = int(output_dict['num_detections'][0])
-			output_dict['detection_classes'] = output_dict[
-				'detection_classes'][0].astype(np.int64)
-			output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
-			output_dict['detection_scores'] = output_dict['detection_scores'][0]
+	# all outputs are float32 numpy arrays, so convert types as appropriate
+	output_dict['num_detections'] = int(output_dict['num_detections'][0])
+	output_dict['detection_classes'] = output_dict[
+		'detection_classes'][0].astype(np.int64)
+	output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
+	output_dict['detection_scores'] = output_dict['detection_scores'][0]
 	return output_dict
 
+def resource_cleanup(tensorflow_graph, cv2_resource, sess):
+	sess.close()
+	tensorflow_graph.close()
+	cv2_resource.release()
 
 cap = cv2.VideoCapture('test2.mp4')
-# cap.set(cv2.CAP_PROP_FPS, 10)
+detection_graph.as_default()
+sess = tf.Session()
 
 while True:
 	ret, image_np = cap.read()
@@ -62,7 +63,7 @@ while True:
 	# Expand dimensions since the model expects images to have shape: [1, None, None, 3]
 	image_np_expanded = np.expand_dims(cv2.resize(image_np, (300,300)), axis=0)
 	# Actual detection.
-	output_dict = run_inference_for_single_image(image_np_expanded, detection_graph)
+	output_dict = run_inference_for_single_image(image_np_expanded, detection_graph, sess)
 	# Visualization of the results of a detection.
 	vis_util.visualize_boxes_and_labels_on_image_array(
 		image_np,
@@ -73,8 +74,9 @@ while True:
 		instance_masks=output_dict.get('detection_masks'),
 		use_normalized_coordinates=True,
 		line_thickness=8)
-	cv2.imshow('object detection', image_np)
+	cv2.imshow('object detection', cv2.resize(image_np, (800,600)))
 	if cv2.waitKey(1) & 0xFF == ord('q'):
+		resource_cleanup(detection_graph, cap, sess)
 		cap.release()
 		cv2.destroyAllWindows()
 		break
